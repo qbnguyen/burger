09a78bda4092bc796f9e969a4f0a37fd066df737
Quy Nguyen <nguyenqb24@gmail.com>
2018-11-06 22:54:12 -0500

    deployment

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 var mysql = require("mysql");
+var connection;
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 var connection = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
